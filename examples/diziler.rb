5a960a272d309ce219ad88bed399ee85c7ebb5d3
jhalitaksoy <jhalitaksoy>
2018-01-13 01:18:06 +0300

    Diziler
    --> diziler.rb

---
+++ examples/diziler.rb
@@ -0,0 +1,13 @@
+ary = [12, "text", 3.6, 4] #array (dizi) oluşturduk
+puts ary # ve yazdırdık
+
+puts
+puts ary[0] #  dizinin 0 indisindeki elemanı yazdırdık
+
+colors = {"red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f} # ruby de hash yapısı
+
+puts
+puts colors
+
+#arraylari ve hashleri daha ayrıntılı öğrenicem
+
