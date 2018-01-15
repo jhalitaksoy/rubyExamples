f69c48517beaaa8dfd75e13222c0c2c5a1835d34
jhalitaksoy <jhalitaksoy>
2018-01-15 13:16:27 +0300

    Ranges
    --> ranges.rb

---
+++ examples/ranges.rb
@@ -0,0 +1,44 @@
+
+puts (1..5).to_a
+
+puts
+puts (1...5).to_a
+
+puts
+puts ('a'...'d').to_a
+
+
+
+
+
+
+# Assume a range
+digits = 0..9
+
+puts digits.include?(5)
+ret = digits.min
+puts "Min value is #{ret}"
+
+ret = digits.max
+puts "Max value is #{ret}"
+
+ret = digits.reject {|i| i < 5 }
+puts "Rejected values are #{ret}"
+
+digits.each do |digit|
+   puts "In Loop #{digit}"
+end
+
+
+
+score = 70
+
+result = case score
+   when 0..40 then "Fail"
+   when 41..60 then "Pass"
+   when 61..70 then "Pass with Merit"
+   when 71..100 then "Pass with Distinction"
+   else "Invalid Score"
+end
+
+puts result
